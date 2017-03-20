5552bfd86f3941dbf13ecfc065102ee5a8a13e75
botcs <botos.official@gmail.com>
2017-03-20 01:32:42 +0100

    WORKING assemble of modules, data flow OK, refactored FLAGS

--- tensorflow/model/classifier.py
+++ tensorflow/model/classifier.py
@@ -2,25 +2,22 @@
 # coding: utf-8
 
 import tensorflow as tf
-import numpy as np
-import time
-import os
 
-def_input_dim = 1 # Scalar
-# normal sinus rhythm, atrial fibrillation (AF), an alternative rhythm, or is too noisy
-def_label_dim = 4 
-def_keep_prob = 0.5
+class foo(object):
+    pass
+FLAGS = foo()
+FLAGS.input_dim = 1 # Scalar
+FLAGS.label_dim = 4 
+FLAGS.keep_prob = 0.5
+FLAGS.fc_sizes = '30, 10'
+'''
 # Model Hyperparameters
-
 # FOR SCRIPTING
 flags = tf.app.flags
-
-flags.DEFINE_string('fc_sizes', '30, 10', 'Size of fully connected layers. Use comma separated integers ["30, 10"]')
-flags.DEFINE_float('keep_prob', def_keep_prob, 'Probability of keeping an activation value after the DROPOUT layer, during training [%f]'%def_keep_prob)
-flags.DEFINE_string('model_path', '/tmp/model', 'Logs will be saved to this directory')
-
+flags.DEFINE_string('fc_sizes', FLAGS.fc_sizes, 'Size of fully connected layers. Use comma separated integers [%s]'%FLAGS.fc_sizes)
+flags.DEFINE_float('keep_prob', FLAGS.keep_prob, 'Probability of keeping an activation value after the DROPOUT layer, during training [%f]'%FLAGS.keep_prob)
 FLAGS = flags.FLAGS
-
+'''
 class model(object):
     '''
     Classify fixed length features, with weighted loss
@@ -29,24 +26,25 @@ class model(object):
     '''
     def get_input(self):
         with tf.variable_scope('input'):
-            x = tf.placeholder(tf.float32, [None, def_input_dim], name='input')
+            x = tf.placeholder(tf.float32, [None, FLAGS.input_dim], name='input')
         return x
 
-    def get_fc(self, in_node, fc_sizes, out_dim, keep_prob):
+    def get_layers(self, in_node, fc_sizes, out_dim, keep_prob):
         with tf.variable_scope('fully_connected'):
             act_fn = tf.nn.relu
             h = in_node
             keep_prob = tf.placeholder_with_default(keep_prob, [], 'keep_prob')
             for size in fc_sizes:
                 h = tf.contrib.layers.fully_connected(h, size, act_fn)
-                tf.nn.dropout(h, keep_prob)
-            preds = tf.contrib.layers.fully_connected(h, out_dim, None)
-        return preds, keep_prob
+                print(h)
+                h = tf.nn.dropout(h, keep_prob)
+            logits = tf.contrib.layers.fully_connected(h, out_dim, None, scope='logits')
+            print(logits)
+        return logits, keep_prob
     
     def get_name(self):
         fc_sizes = [str(s) for s in self.fc_sizes]
         name = '--fc' + '-'.join(fc_sizes)
-        name += '---' + time.strftime("%Y-%m-%d")
         return name
     
     def build_graph(self, model_name=None):
@@ -59,19 +57,16 @@ class model(object):
         if self.input is None:
             self.input = self.get_input()
         
-        self.logits, self.fc_keep_prob = self.get_fc(
-            self.input, self.fc_sizes, def_label_dim, self.keep_prob)
+        self.logits, self.fc_keep_prob = self.get_layers(
+            self.input, self.fc_sizes, FLAGS.label_dim, self.keep_prob)
 
-        self.pred = tf.nn.softmax(logits=self.logits)
-    
-    def get_checkpoint_path(self):
-        return os.path.join(self.model_path, self.name)
+        self.pred = tf.nn.softmax(logits=self.logits, name='pred')
+        print(self.pred)
         
     def __init__(self,
             input=None,
             fc_sizes=[int(s) for s in FLAGS.fc_sizes.split(',')],
             keep_prob=FLAGS.keep_prob,
-            model_path=FLAGS.model_path,
             model_name=None):
         '''
         Initializer default vales use tf.app.flags
@@ -79,13 +74,13 @@ class model(object):
         
         fc_sizes: [int, [int...]] Size of fc layers connected to the last LSTM cell's output
         keep_prob: float, Probability of keeping a value in DROPOUT layers
-        model_path: str, path/to/model/dir
         '''
         self.input = input
         self.fc_sizes = fc_sizes
         self.def_keep_prob = keep_prob
-        self.model_path = model_path
+        self.name = self.get_name()
         with tf.variable_scope('classifier'):
+            print('\nFC' + self.name)
             self.build_graph()
 
 def get_logits_and_pred(input, **kwargs):
--- tensorflow/model/cnn.py
+++ tensorflow/model/cnn.py
@@ -2,28 +2,26 @@
 # coding: utf-8
 
 import tensorflow as tf
-import time
-import os
 
-def_use_bnorm = True
-
-def_kernel_size = 5
-
+class foo(object):
+    pass
+FLAGS = foo()
+FLAGS.use_bnorm = True
+FLAGS.kernel_size = 5
+FLAGS.keep_prob = 0.5
+FLAGS.out_dims = '512, 1024'
+FLAGS.kernel_sizes = '128, 64'
 # normal sinus rhythm, atrial fibrillation (AF), an alternative rhythm, or is too noisy
-def_keep_prob = 0.5
+'''
 # Model Hyperparameters
-
 # FOR SCRIPTING
 flags = tf.app.flags
-flags.DEFINE_string('out_dims', '512, 1024', 'Size of feature map dimensions. Use comma separated integers ["30, 10"]')
-flags.DEFINE_string('kernel_sizes', '128, 64', 'Size of convolution kernels. Use comma separated integers ["128, 64"]')
-flags.DEFINE_float('keep_prob', def_keep_prob, 'Probability of keeping an activation value after the DROPOUT layer, during training [%f]'%def_keep_prob)
-flags.DEFINE_bool('use_bnorm', def_use_bnorm, 'Use batch normalization if True, else use simply biases')
-flags.DEFINE_string('model_path', '/tmp/model', 'Logs will be saved to this directory')
+flags.DEFINE_string('out_dims', FLAGS.out_dims, 'Size of feature map dimensions. Use comma separated integers [%s]'%FLAGS.out_dims)
+flags.DEFINE_string('kernel_sizes', FLAGS.out_dims, 'Size of convolution kernels. Use comma separated integers [%s]'%FLAGS.out_dims)
+flags.DEFINE_float('keep_prob', FLAGS.keep_prob, 'Probability of keeping an activation value after the DROPOUT layer, during training [%f]'%FLAGS.keep_prob)
+flags.DEFINE_bool('use_bnorm', def_use_bnorm, 'Use batch normalization if True, else use simply biases [%b]'%FLAGS.use_bnorm)
 FLAGS = flags.FLAGS
-
-
-# In[18]:
+'''
 
 class model(object):
     '''
@@ -36,7 +34,7 @@ class model(object):
         x = tf.placeholder(tf.float32, [None, None], name='input')
         return x
 
-    def get_cnn(self, in_node, out_dims, kernel_sizes, keep_prob, use_bnorm=True):
+    def get_layers(self, in_node, out_dims, kernel_sizes, keep_prob, use_bnorm=True):
         '''
         `out_dims`: a list of integers for the featuremap [out_dims1, out_dims2, ...]
         `kernels_sizes`: a single integer or 
@@ -60,13 +58,11 @@ class model(object):
             
             for dim, ker in zip(out_dims, kernel_sizes):
                 # does the same as 1d, but with convenience function
-                print('\n', h)
                 h = tf.contrib.layers.conv2d(h, dim, ker, 
                                              normalizer_fn=normalizer_fn,
                                              biases_initializer=biases_initializer)
-                
-                h = tf.nn.dropout(h, keep_prob)
                 print(h)
+                h = tf.nn.dropout(h, keep_prob)
         return h
     
     def get_name(self):
@@ -74,7 +70,6 @@ class model(object):
             zip(self.out_dims, self.kernel_sizes)]
         
         name = '--cnn' + '-'.join(cnn_sizes)
-        name += '---' + time.strftime("%Y-%m-%d")
         return name
     
     def build_graph(self, model_name=None):
@@ -87,26 +82,19 @@ class model(object):
         if self.input is None:
             self.input = self.get_input()
         
-        self.output = self.get_cnn(
+        self.output = self.get_layers(
             self.input, self.out_dims, self.kernel_sizes, self.keep_prob)
     
-    def get_checkpoint_path(self):
-        return os.path.join(self.model_path, self.name)
         
     def __init__(self,
             input=None,
             out_dims=[int(s) for s in FLAGS.out_dims.split(',')],
             kernel_sizes=[int(s) for s in FLAGS.kernel_sizes.split(',')],
             keep_prob=FLAGS.keep_prob,
-            model_path=FLAGS.model_path,
             model_name=None):
         '''
         Initializer default vales use tf.app.flags
         returns an object, whose main fields are tensorflow graph nodes.
-        
-        fc_sizes: [int, [int...]] Size of fc layers connected to the last LSTM cell's output
-        keep_prob: float, Probability of keeping a value in DROPOUT layers
-        model_path: str, path/to/model/dir
         '''
         self.input = input
         self.out_dims = out_dims
@@ -114,12 +102,12 @@ class model(object):
         
         if len(kernel_sizes) == 1:
             kernel_sizes = [kernel_sizes] * len(out_dims)
-        
         self.def_keep_prob = keep_prob
-        self.model_path = model_path
+        self.name = self.get_name()
         with tf.variable_scope('CNN'):
+            print('\nCNN' + self.name)
             self.build_graph()
 
-def get_cnn_output(input, **kwargs):
+def get_output(input, **kwargs):
     cnn = model(input, **kwargs)
-    return model.output
+    return cnn.output
