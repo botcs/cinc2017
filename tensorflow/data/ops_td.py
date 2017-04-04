649797ba44f8bca868f9260163598a03315eddc7
horan65 <horvath.andras@itk.ppke.hu>
2017-04-04 23:42:12 +0200

    Tf record writer and sequencer which contains the previously saved time domain features

---
+++ tensorflow/data/ops_td.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env ipython
+# coding: utf-8
+
+import tensorflow as tf
+import numpy as np
+
+class foo(object):
+    pass
+FLAGS = foo()
+FLAGS.path = 'train.TFRecord'
+FLAGS.batch_size = tf.constant(16, name='batch_size')
+FLAGS.capacity = 512
+FLAGS.threads = 8
+
+def parse_example(filename_queue):
+    # Define how to parse the example
+    
+    reader = tf.TFRecordReader()
+    _, example = reader.read(filename_queue)
+    
+    context_features = {
+        'length': tf.FixedLenFeature([], dtype=tf.int64),
+        'label': tf.FixedLenFeature([], dtype=tf.int64)
+    }
+    sequence_features = {
+        'data': tf.FixedLenSequenceFeature([], dtype=tf.float32),
+        'tdfeature': tf.FixedLenSequenceFeature([], dtype=tf.float32)
+    }
+    con_parsed, seq_parsed = tf.parse_single_sequence_example(
+        serialized=example,
+        context_features=context_features,
+        sequence_features=sequence_features
+    )
+    res = (seq_parsed['data'], 
+        con_parsed['length'], 
+        con_parsed['label'],
+        seq_parsed['tdfeature'])
+    return res
+
+def get_batch_producer(
+    path=FLAGS.path, 
+    batch_size=FLAGS.batch_size, 
+    prefetch_size=FLAGS.capacity,
+    num_of_threads=FLAGS.threads,
+    scope='batch_producer'):
+    with tf.name_scope(scope):
+        filename_queue = tf.train.string_input_producer([path], name='filename_producer')
+        with tf.name_scope('example_producer'):
+            data, seq_len, label, td_feature = parse_example(filename_queue)
+            data = tf.placeholder_with_default(data, [None], name='data')
+            label = tf.cast(label, tf.int32, name='label')
+            seq_len = tf.cast(seq_len, tf.int32, name='seq_length')
+            td_feature = tf.placeholder_with_default(td_feature, [None], name='td_feature')
+        with tf.name_scope('padded_batch_producer'):
+            q = tf.PaddingFIFOQueue(
+                capacity=prefetch_size,
+                dtypes=[tf.float32, tf.int32, tf.int32, tf.float32],
+                shapes=[[None], [], [], [None]], name='padding_queue')
+
+            enqueue_op = q.enqueue([data, seq_len, label, td_feature], name='push_single_example')
+            qr = tf.train.QueueRunner(q, [enqueue_op]*num_of_threads)
+            tf.train.add_queue_runner(qr)
+            batch_op = q.dequeue_many(n=batch_size, name='pop_batch')
+    return batch_op
---
+++ tensorflow/data/writer_with_td_features.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env ipython
+# coding: utf-8
+
+import numpy as np
+import tensorflow as tf
+import scipy.io as io
+import random
+
+def read_raw(UsedForTraining,dir='../../training2017/'):
+    UsedForTesting=1.0-UsedForTraining
+    label_dict = {
+        'N':0,
+        'A':1,
+        'O':2,
+        '~':3
+    }
+
+
+    #load presaved features - time domain features based on pantompkins
+    features=np.load('td_features.npy') #this file contains the previously saved time domain features
+    train_data = []
+    train_features = []
+    train_label = []
+    test_data = []
+    test_label = []
+    test_features = []
+    lens = []
+    annotations = open(dir+'REFERENCE.csv', 'r').read().splitlines()
+    NumRecords=len(annotations)
+    Trainindices=set(random.sample(range(NumRecords),int(NumRecords*UsedForTraining)))
+    for i, line in enumerate(annotations):
+        fname, label_str = line.split(',')
+
+        x = io.loadmat(dir+fname+'.mat')['val']\
+            .astype(np.float32).squeeze()
+        # Normal
+        x -= x.mean()
+        x /= x.std()
+
+        
+
+        y = label_dict[label_str]
+        if i in Trainindices:
+            train_label.append(y)
+            train_data.append(x)
+            train_features.append(features[i,:])
+        else:
+            test_label.append(y)
+            test_data.append(x)
+            test_features.append(features[i,:])
+        lens.append(len(x))
+        if i%50==0: 
+            print('\rReading files: %05d   ' % i, end='', flush=True)
+
+    print('\rReading files: %05d   ' % i, end='', flush=True)
+    assert(len(train_label) == len(train_data))
+    assert(len(test_label) == len(test_data))
+    print('\nReading successful!')
+    
+    train_data = np.array(train_data)
+    train_label = np.array(train_label)
+    train_features =np.array(train_features)
+    test_data = np.array(test_data)
+    test_label = np.array(test_label)
+    test_features =np.array(test_features)
+    #lens = np.array(lens)  # it seems we do not use this at all???
+    #data_size = len(data)
+    train_class_hist = np.histogram(train_label, bins=len(label_dict))[0]
+    test_class_hist = np.histogram(test_label, bins=len(label_dict))[0]
+    
+    return train_data, train_label, train_class_hist,train_features, test_data, test_label, test_class_hist, test_features
+
+def make_example(sequence, label, features):
+    # The object we return
+    ex = tf.train.SequenceExample()
+    # A non-sequential feature of our example
+    sequence_length = len(sequence)
+    ex.context.feature['length'].int64_list.value.append(sequence_length)
+    ex.context.feature['label'].int64_list.value.append(label)
+
+
+    fl_val = ex.feature_lists.feature_list['tdfeature']
+    for token in features:
+        fl_val.feature.add().float_list.value.append(token)
+
+
+    fl_val = ex.feature_lists.feature_list['data']
+    for token in sequence:
+        fl_val.feature.add().float_list.value.append(token)
+
+    return ex
+
+
+def write_TFRecord(data, label, features, fname='train', threads=8):
+    with open(fname + '.TFRecord', 'w') as fp:
+        writer = tf.python_io.TFRecordWriter(fp.name)
+        print('Sampling...')
+         
+            
+        for i, (x, y,z) in enumerate(zip(data, label,features)):
+            ex = make_example(x, y,z)
+            writer.write(ex.SerializeToString())
+            print('\r%05d'%i, end=' ', flush=True)
+        writer.close()
+        print("\nWrote to {}".format(fp.name))
+        
+if __name__ == '__main__':
+    UsedForTraining=0.7
+    train_data, train_label, train_class_hist,train_features, test_data, test_label, test_class_hist, test_features = read_raw(UsedForTraining)
+    np.save('train_class_hist_with_td_features', train_class_hist)
+    write_TFRecord(train_data, train_label,train_features,fname='train_with_td_features')
+    print('train sampling finished')
+    np.save('test_class_hist_with_td_features', test_class_hist)
+    write_TFRecord(test_data, test_label,test_features,fname='test_with_td_features')
