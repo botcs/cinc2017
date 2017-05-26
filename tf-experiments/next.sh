d1f725dde2862236abdef4ca75d0452cd6104483
Botos Csaba <botos.official@gmail.com>
2017-05-26 12:01:10 +0200

    Sequential next.sh added

--- tf-experiments/next.sh
+++ tf-experiments/next.sh
@@ -3,7 +3,18 @@
 is_running="$(ps aux | grep entry_eval_ppkeitk.py | wc -l)"
 # echo $is_running
 
+echo "Add $1 to RECORDS"
 echo $1 >> RECORDS
 if [ $is_running -ne 2 ]; then
+  echo "restart background process"
   python3.5 entry_eval_ppkeitk.py &
+  sleep 10
 fi
+
+is_ready="$(cat answers.txt | grep $1 | wc -l)"
+while [ $is_ready -lt 1 ]; do
+  echo "waiting for $1"
+  sleep .1
+  is_ready="$(cat answers.txt | grep $1 | wc -l)"
+done
+echo "ready"
