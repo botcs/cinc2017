096f4a0b06e5261b0fcd69464efad7ac744ab387
Botos Csaba <botos.official@gmail.com>
2017-05-26 13:47:32 +0200

    Reformatted saver

--- tf-experiments/entry_eval_ppkeitk.py
+++ tf-experiments/entry_eval_ppkeitk.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3.5
 
-from tensorflow.python import debug as tf_debug
+# from tensorflow.python import debug as tf_debug
 import json
 import os
+import sys
 import numpy as np
 from scipy.io import loadmat
 import tensorflow as tf
@@ -10,6 +11,10 @@ from time import time, sleep
 
 from model.assembler import get_model_logits
 tf.set_random_seed(42)
+
+
+def pprint(*text, **kwargs):
+    print('---TFLOW:', *text, flush=True, **kwargs)
 
 
 class data_handler:
@@ -58,10 +63,10 @@ class data_handler:
         assert fname not in self.processed
         assert fname == self.processing
 
-        print(sample['name'], 'evaluated to',
-              dh.map_logits(logits),
-              (np.exp(logits)/np.exp(logits).sum() * 100).astype(int),
-              'Wall time: %4.3f' % (time() - self.start_time))
+        pprint(fname, 'evaluated to',
+               self.map_logits(logits),
+               (np.exp(logits)/np.exp(logits).sum() * 100).astype(int),
+               'Wall time: %4.3f' % (time() - self.start_time))
         with open(self.answers, 'a') as f:
             f.write(fname + ',' + self.map_logits(logits) + '\n')
 
@@ -70,68 +75,87 @@ class data_handler:
         self.last_update = time()
 
     def check(self):
-        with open('RECORDS') as f:
-            fnames = {fn for fn in f.read().split('\n') if len(fn) > 0}
+        try:
+            with open('RECORDS') as f:
+                fnames = {fn for fn in f.read().split('\n') if len(fn) > 0}
+        except FileNotFoundError:
+            pass
 
         new_names = fnames - self.processed - self.waiting
         if len(new_names) > 0:
-            print('New files (%d) are waiting' % len(new_names))
+            pprint('New files (%d) read' % len(new_names))
             self.last_update = time()
             self.waiting.update(new_names)
             return True
         elif len(self.waiting) > 0:
-            print('No new file, but (%d) are waiting' %
-                  len(self.waiting))
+            pprint('No new file, but (%d) are waiting' %
+                   len(self.waiting))
             return True
         return False
 
 
-dh = data_handler('data/raw/training2017/')
-proto_path = 'proto/small_resnet.json'
-proto_name = os.path.splitext(proto_path)[0]
-proto_name = os.path.basename(proto_name)
+def main(argv):
 
-tf.reset_default_graph()
-print('Building computational graph... ')
-print('-'*80)
-network_params = json.load(open(proto_path))
-input_op = tf.placeholder(1, [1, None], 'INPUT')
-seq_len = tf.shape(input_op, name='seq_len')[-1]
-logits = get_model_logits(seq_len, input_op, **network_params)[0]
-print('-'*80)
-print('Computational graph building done!')
+    dh = data_handler('data/raw/training2017/')
+    proto_path = 'proto/deep_resnet_no_pool.json'
+    proto_name = os.path.splitext(proto_path)[0]
+    proto_name = os.path.basename(proto_name)
 
-print('Loading pretrained parameters...')
-sv = tf.train.Supervisor(
-    logdir='ckpt/%s/' % proto_name,
-)
+    tf.reset_default_graph()
+    pprint('Building computational graph... ')
+    pprint('-'*80)
+    network_params = json.load(open(proto_path))
+    input_op = tf.placeholder(1, [1, None], 'INPUT')
+    seq_len = tf.shape(input_op, name='seq_len')[-1]
+    logits = get_model_logits(seq_len, input_op, **network_params)[0]
+    pprint('-'*80)
+    pprint('Computational graph building done!')
 
-WAIT_TIME = .1
-MAX_TIME = 10.
+    pprint('Loading pretrained parameters...')
 
-'''
-if tf.gfile.Exists('answers.txt'):
-    print('!!!REMOVING previous answers.txt')
-    tf.gfile.Remove('answers.txt')
-'''
+    WAIT_TIME = .01
+    MAX_TIME = 10.
 
-with sv.managed_session() as sess:
-    # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-    start_time = time()
-    t = time() - start_time
-    while not sv.should_stop():
-        print('Checking RECORDS:', end='')
-        if dh.check():
-            while len(dh.waiting) > 0:
-                sample = dh.next()
-                feed = {'is_training:0': False, input_op: sample['val']}
-                logits_val = sess.run(logits, feed)
-                dh.write(sample['name'], logits_val)
+    '''
+    if tf.gfile.Exists('answers.txt'):
+        pprint('!!!REMOVING previous answers.txt')
+        tf.gfile.Remove('answers.txt')
+    '''
+
+    with tf.Session() as sess:
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+        ckpt = tf.train.get_checkpoint_state('ckpt/%s/' % proto_name)
+        saver = tf.train.Saver()
+        if ckpt and ckpt.model_checkpoint_path:
+            # Restores from checkpoint
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            # Assuming model_checkpoint_path looks something like:
+            #   /my-favorite-path/cifar10_train/model.ckpt-0,
+            # extract global_step from it.
+            global_step =\
+                ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            pprint('Checkpoint loaded at train step:', global_step)
         else:
-            print('No file to process, waiting %1.1f sec(s)...' %
-                  WAIT_TIME)
-            sleep(WAIT_TIME)
-        if dh.t_since_update() > MAX_TIME:
-            break
+            pprint('No checkpoint file found')
+            return
 
-    print('Max idle time (%d secs) expired, quitting' % MAX_TIME)
+        start_time = time()
+        t = time() - start_time
+        while True:
+            if dh.check():
+                while len(dh.waiting) > 0:
+                    sample = dh.next()
+                    feed = {'is_training:0': False, input_op: sample['val']}
+                    logits_val = sess.run(logits, feed)
+                    dh.write(sample['name'], logits_val)
+            else:
+                # pprint('No file to process, waiting... ')
+                sleep(WAIT_TIME)
+            if dh.t_since_update() > MAX_TIME:
+                break
+
+        pprint('Max idle time (%d secs) expired, quitting' % MAX_TIME)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- tf-experiments/next.sh
+++ tf-experiments/next.sh
@@ -3,18 +3,17 @@
 is_running="$(ps aux | grep entry_eval_ppkeitk.py | wc -l)"
 # echo $is_running
 
-echo "Add $1 to RECORDS"
+echo "+++SHELL: Add $1 to RECORDS"
 echo $1 >> RECORDS
 if [ $is_running -ne 2 ]; then
-  echo "restart background process"
-  python3.5 entry_eval_ppkeitk.py &
+  echo "+++SHELL: Restart background process"
+  ipython3 entry_eval_ppkeitk.py &
   sleep 10
 fi
 
 is_ready="$(cat answers.txt | grep $1 | wc -l)"
 while [ $is_ready -lt 1 ]; do
-  echo "waiting for $1"
-  sleep .1
+  # echo "+++SHELL: Waiting for $1"
+  sleep .01
   is_ready="$(cat answers.txt | grep $1 | wc -l)"
 done
-echo "ready"
