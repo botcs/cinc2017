31eaa469bd72b514ed9036dfeaf7bf87b035e05a
Botos Csaba <botos.official@gmail.com>
2017-05-26 16:37:59 +0200

    FIX: python3 syntax checked

--- tf-experiments/model/assembler.py
+++ tf-experiments/model/assembler.py
@@ -16,11 +16,12 @@ def get_model_logits(seq_len, input_op, **params):
     if params.get('resnet'):
         with tf.name_scope('ResNet'):
             var_features = resnet.get_resnet_output(
-                *var_features, **params['resnet'])
+                var_features[0], var_features[1], **params['resnet'])
 
     elif params.get('fcn'):
         var_features = cnn.get_output(
-            *var_features, return_name=False, **params['fcn'])
+            var_features[0], var_features[1],
+            return_name=False, **params['fcn'])
 
     ####################################
     # Fixed length feature extraction
@@ -31,7 +32,7 @@ def get_model_logits(seq_len, input_op, **params):
         # Use variable sized windows to make equal fractions of input
         # Preserving the raw locality of the features
         var_features = variable_size_window(
-            *var_features, params['partition_num'])
+            var_features[0], var_features[1], params['partition_num'])
         # seq_len is no longer needed, therefore discarded
         features = tf.reduce_mean(var_features[1], axis=2)
         tf.add_to_collection('activations', features)
@@ -39,7 +40,7 @@ def get_model_logits(seq_len, input_op, **params):
         print(features)
     else:
         raise ValueError('No fixed length feature extraction specified')
-    
+
     classifier_in = tf.contrib.layers.flatten(features)
     fc_params = params['fc_params']
     logits = classifier.get_logits_and_pred(
