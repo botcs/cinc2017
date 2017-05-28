3751fe7b4097aba715fe7c65f3919563982b03f1
Botos Csaba <botos.official@gmail.com>
2017-05-28 15:57:50 +0200

    latest try

--- tf-experiments/next.sh
+++ tf-experiments/next.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-is_running="$(ps aux | grep entry_eval_ppkeitk.py | wc -l)"
-# echo $is_running
-
-echo $1 >> RECORDS
-if [ $is_running -ne 2 ]; then
-  python3.5 entry_eval_ppkeitk.py &
-fi
+ln -sf **/$1.mat .
+python3 entry_eval_ppkeitk.py $1
--- tf-experiments/train_model_from_template.py
+++ tf-experiments/train_model_from_template.py
@@ -9,15 +9,21 @@ from model import train_ops
 from model import summary_ops
 import data.ops
 
+
 def main(argv):
+
+    EPOCHS = 1000
+    BATCH_SIZE = 8
+    TRAIN_STEPS = (EPOCHS * 8525) // BATCH_SIZE
+
     proto_path = argv[-1]
-    #proto_path = 'small_resnet.json'
+    # proto_path = 'small_resnet.json'
     proto_name = os.path.splitext(proto_path)[0]
     proto_name = os.path.basename(proto_name)
 
     tf.reset_default_graph()
     network_params = json.load(open(proto_path))
-    bsize = tf.placeholder_with_default(128, [], 'batch_size')
+    bsize = tf.placeholder_with_default(BATCH_SIZE, [], 'batch_size')
     input_op, seq_len, label_op = data.ops.get_batch_producer(
         './data/TFRecords/aug_train.TFRecord', bsize)
 
@@ -25,14 +31,11 @@ def main(argv):
     summary_ops.add_label_hist(label_op)
     logits = get_model_logits(seq_len, input_op, **network_params)
 
-    t_ops = train_ops.get_trainer(label_op, logits)
-    step, train, loss, conf, acc, lrate, loss_avg, ver_avg = t_ops
+    t_ops = train_ops.get_trainer(label_op, logits, batch_size=bsize)
+    step, train, loss, conf, acc, lrate = t_ops
 
     tf.summary.histogram('sample_diversity', label_op)
     s_op = summary_ops.get_all_summaries(*t_ops)
-    
-    TRAIN_STEPS = 30000
-
 
     sv = tf.train.Supervisor(
         logdir='./training/%s/' % proto_name, summary_op=s_op,
@@ -41,7 +44,7 @@ def main(argv):
         it = 0
         while not sv.should_stop() and it < TRAIN_STEPS:
             it, loss_val, acc_val, _ = sess.run([step, loss, acc, train])
-            print('it:%d, loss:%02.4f, acc:%02.4f'%(it, loss_val, acc_val))
+            print('it:%d, loss:%02.4f, acc:%02.4f' % (it, loss_val, acc_val))
 
 if __name__ == '__main__':
     main(sys.argv)
