a39b3839fbf0958ca04ce1bdd4b26f02015aa3a6
Botos Csaba <botos.official@gmail.com>
2017-06-28 14:33:42 +0200

    Remove TF experiments

--- tf-experiments/next.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ln -sf **/$1.mat .
-python3 entry_eval_ppkeitk.py $1
--- tf-experiments/train.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-name=$(basename "$1" ".json")
-nohup python3.5 train_model_from_template.py "$1"  >> logs/$name &
-echo 
