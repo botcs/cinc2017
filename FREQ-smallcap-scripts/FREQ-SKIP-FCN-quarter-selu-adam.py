e427dcb5aa7bc26454a72f7619a77bd2c1d437e0
Botos Csaba <botos.official@gmail.com>
2017-07-31 00:48:34 +0200

    Pretrain commit: tryhard to make results from reduced capacity freq models

---
+++ FREQ-smallcap-scripts/FREQ-SKIP-FCN-quarter-selu-adam.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3.5
+import data_handler
+import dilated_model as DM
+import trainer as T
+import numpy as np
+import torch as th
+from torch.autograd import Variable
+import sys, os
+os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
+from os.path import basename, splitext
+
+DRYRUN = '--dryrun' in sys.argv
+RESTORE = '--restore' in sys.argv
+print('DRYRUN:', DRYRUN, '\tRESTORE:', RESTORE)
+
+th.multiprocessing.set_sharing_strategy('file_system')
+name = splitext(basename(sys.argv[0]))[0]
+
+transformations = [
+    data_handler.Crop(8000),
+    data_handler.RandomMultiplier(-1),
+    data_handler.Spectogram(15),
+    data_handler.Logarithm()
+]
+dataset = data_handler.DataSet(
+    'data/raw/training2017/REFERENCE.csv', data_handler.load_composed,
+    transformations=transformations,
+    path='data/raw/training2017/',
+    remove_noise=True, tokens='NAO')
+train_set, eval_set = dataset.disjunct_split(.9)
+train_producer = th.utils.data.DataLoader(
+        dataset=train_set, batch_size=256, shuffle=True,
+        num_workers=4, collate_fn=data_handler.batchify)
+test_producer = th.utils.data.DataLoader(
+        dataset=eval_set, batch_size=256, shuffle=True,
+        num_workers=4, collate_fn=data_handler.batchify)
+
+net = DM.SkipFCN(in_channel=8, use_selu=True,
+    channels=[8,8,  16,16,  32,32,32,  32,32,32,  64,64,64])
+
+print(net(next(iter(train_producer))['x']).size())
+
+trainer = T.Trainer('saved/'+name, restore=RESTORE, dryrun=DRYRUN)
+trainer(net, train_producer, test_producer, gpu_id=0, useAdam=True)
---
+++ launch-0730-FREQ-smallcap.sh
@@ -0,0 +1,7 @@
+nohup ./FREQ-ENCODE-halved-selu-adam.py 0 &
+nohup ./FREQ-ENCODE-halved-selu-adam.py 1 &
+nohup ./FREQ-ENCODE-octed-selu-adam.py 2 &
+nohup ./FREQ-ENCODE-quarter-selu-adam.py 3 &
+nohup ./FREQ-ENCODE-quarter-selu-adam.py 4 &
+nohup ./FREQ-SKIP-RES-halved-selu-adam.py 5 &
+nohup ./FREQ-SKIP-RES-halved-selu-adam.py 6 &
