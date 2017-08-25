d43bdab4c2bd14a1e1a93d0682627d79889e25e0
Botos Csaba <botos.official@gmail.com>
2017-08-25 12:58:33 +0200

    Preparing final models

--- tf-experiments/data/download_raw.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-url="https://physionet.org/challenge/2017/training2017.zip"
-mkdir -p "raw"
-cd "raw"
-wget -nc $url -O "training2017.zip"
-unzip -nq "training2017.zip"
-#rm "training2017.zip" -rf
