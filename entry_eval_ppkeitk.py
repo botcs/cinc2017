4999df9b7216fc53c4144ef999b2dc1012279865
Botos Csaba <botos.official@gmail.com>
2017-08-26 12:10:15 +0200

    Almost ready for submit

---
+++ entry_eval_ppkeitk.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+import tensorflow_wrapper as tw
+import data_handler
+
+def pprint(*text, **kwargs):
+    print(*text, flush=True, **kwargs)
+
+
+def write_answer(fname, logits):
+    labels = ['N', 'A', 'O', '~']
+    with open('answers.txt', 'a') as f:
+        f.write(fname + ',' + labels[int(np.argmax(logits))] + '\n')
+
+def main(argv):
+    noise_model_path = 'noise.pt'
+    atrif_model_path = 'atrif.pt'
+    other_model_path = 'other.pt'
+
+    tf.reset_default_graph()
+    pprint('Building computational graph... ')
+    pprint('-'*80)
+
+    input_op = tf.placeholder(1, [1, None, 1], name='INPUT')
+    with tf.device('cpu:0'):
+        noise_model = tw.get_logits(input_op, 3, noise_model_path)
+        atrif_model = tw.get_logits(input_op, 3, atrif_model_path)
+        other_model = tw.get_logits(input_op, 3, other_model_path)
+        logits = [noise_model, atrif_model, other_model]
+    pprint('-'*80)
+    pprint('Computational graph building done!')
+    pprint('Loading data')
+    transformations = [
+            data_handler.Crop(6000),
+            data_handler.Threshold(sigma=2.2),
+            data_handler.RandomMultiplier(-1),
+    ]
+    data = data_handler.load_composed(
+        sys.argv[1], transformations=transformations, only_data=True)[:, :, None]
+    print(data.shape)
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        logits_val = sess.run(logits, {input_op: np.float32(np.random.randn(1, 9000, 1))})
+        print('Noise:', logits_val[0])
+        print('Atrif:', logits_val[1])
+        print('Other:', logits_val[2])
+
+    return
+
+    with tf.Session() as sess:
+        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+        ckpt = tf.train.get_checkpoint_state('ckpt/%s/' % proto_name)
+        saver = tf.train.Saver()
+        if ckpt and ckpt.model_checkpoint_path:
+            # Restores from checkpoint
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            # Assuming model_checkpoint_path looks something like:
+            #   /my-favorite-path/cifar10_train/model.ckpt-0,
+            # extract global_step from it.
+            global_step =\
+                ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            pprint('Checkpoint loaded at train step:', global_step)
+        else:
+            pprint('No checkpoint file found')
+            return
+
+        
+        sample = loadmat(argv[1])
+        feed = {'is_training:0': False, input_op: sample['val']}
+        logits_val = sess.run(logits, feed)
+        write_answer(argv[1], logits_val)
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ tensorflow_wrapper.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import numpy as np
+import torch as th
+from tensorflow.python.framework import ops
+
+def get_logits(input, num_classes, pytorch_statedict_path, res_blocks=3):
+    sd = th.load(pytorch_statedict_path)
+    #for k, v in sd.items():
+    #    print(k, v.size())
+    def gener():
+        for p in sd.values():
+            yield p.cpu().transpose(0, -1).numpy()    
+    paramgen = gener()
+    def init(*args, do_assert=True):
+        p = next(paramgen)
+        if do_assert:
+            assert p.shape == args, (p.shape, args)
+        return p
+
+    def selu(x):
+        with ops.name_scope('elu') as scope:
+            alpha = 1.6732632423543772848170429916717
+            scale = 1.0507009873554804934193349852946
+            return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
+    def BatchNorm(input, channel=8):
+        with tf.variable_scope('BatchNorm'):
+            weight = tf.Variable(init(channel), name='weight')
+            bias = tf.Variable(init(channel), name='bias')
+            mean = tf.Variable(init(channel), name='running_mean')
+            var = tf.Variable(init(channel), name='running_var')
+            return tf.nn.batch_normalization(input, mean, var, bias, weight, 1e-05)
+
+    def Conv1d(input, in_channel, out_channel, kernel_size, dilation=1, bias=False):
+        with tf.variable_scope('Conv1d'):
+            w = tf.Variable(init(kernel_size, in_channel, out_channel), name='weight')
+            if dilation > 1:
+                w = tf.expand_dims(w, 0)
+                x = tf.expand_dims(input, 1)
+                x = tf.nn.atrous_conv2d(x, w, dilation, 'SAME')
+                x = tf.squeeze(x, 1)
+            else:
+                x = tf.nn.conv1d(input, w, 1, 'SAME')
+            if bias:
+                b = tf.Variable(init(out_channel), name='bias')
+                x = x + b
+        return x
+    def MaxPool1d(input):
+        with tf.variable_scope('MaxPool1d'):
+            x = tf.expand_dims(input, 1)
+            x = tf.nn.max_pool(x, [1, 1, 2, 1], [1, 1, 2, 1], 'SAME')
+            x = tf.squeeze(x, 1)
+        return x
+
+    def Encoder(input, init_channel):
+        def DownSampleBlock(input, in_channel, out_channel):
+            with tf.variable_scope('DownSampleBlock'):
+                x = Conv1d(input, in_channel, out_channel, 7, bias=True)
+                x = BatchNorm(x, out_channel)
+                x = selu(x)
+                x = MaxPool1d(x)
+            return x    
+        with tf.variable_scope('Encoder'):
+            x = DownSampleBlock(input, 1, init_channel)
+            x = DownSampleBlock(x, init_channel, init_channel*2)
+            x = DownSampleBlock(x, init_channel*2, init_channel*4)
+            x = DownSampleBlock(x, init_channel*4, init_channel*8)
+        return x
+    def DilatedBlock(input, channel=8, kernel_size=9, dilation=2):
+        # No change in # of channels -> identity mapping
+        with tf.variable_scope('DilatedBlock'):
+            x = BatchNorm(input, channel)
+            x = Conv1d(x, channel, channel, kernel_size)
+            x = selu(x)
+            x = BatchNorm(x, channel)
+            x = Conv1d(x, channel, channel, kernel_size, dilation)
+            x = selu(x)
+        return x + input    
+    def ResNet(input, channel, res_blocks=res_blocks):
+        with tf.variable_scope('ResNet'):
+            x = DilatedBlock(input, channel)
+            for _ in range(res_blocks-1):
+                x = DilatedBlock(x, channel)
+        return x
+
+    def Features(input, init_channel):
+        x = Encoder(input, init_channel)
+        x = ResNet(x, init_channel*8)
+        return x
+
+    def NET(input, init_channel=32, num_classes=num_classes):
+        x = Features(input, init_channel)
+        with tf.variable_scope('Logit'):
+            logit = Conv1d(x, init_channel*8, num_classes, 1)
+            logit = tf.reduce_mean(logit, 1)
+            #print('MEGTÖRTÉNT')
+            
+        return logit
+    return NET(input, init_channel=32)
