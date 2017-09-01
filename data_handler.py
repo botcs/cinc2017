3386185d873be4a4dce0920734b8346f75319c81
csbotos <botos.official@gmail.com>
2017-09-01 12:03:59 +0200

    preparing entry with Combined Model

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,88 @@
 from scipy.io import loadmat
 import numpy as np
-from scipy import signal
 #from matplotlib.mlab import specgram
+from scipy import signal
+#import torch as th
 import random
+'''
+from torch.autograd import Variable
+from torch import optim
+import torchvision
+
+Float = th.FloatTensor
+def_tokens = 'NAO'
+
+
+class mystr(str):
+    def find(self, s):
+        if s == self[0]:
+            return 0
+        return 1
+noise_tokens = mystr('~~')
+atrif_tokens = mystr('AA')
+other_tokens = mystr('OO')
+normal_tokens = mystr('NN')
+
+
+class DataSet(th.utils.data.Dataset):
+    def __init__(self, elems, load, path=None,
+                 remove_unlisted=False, tokens=def_tokens, **kwargs):
+        num_classes = len(tokens)
+        super(DataSet, self).__init__()
+
+        if isinstance(elems, str):
+            with open(elems, 'r') as f:
+                self.list = [line.replace('\n', '') for line in f]
+        else:
+            # just assume iterable
+            self.list = set(elems)
+
+        if kwargs.get('remove_noise'):
+            self.list = [elem for elem in self.list if elem.find('~') == -1]
+
+        if remove_unlisted:
+            self.list = [elem for elem in self.list if tokens.find(elem[-1]) != -1]
+
+        self.class_lists = [[] for _ in range(num_classes)]
+
+        for elem in self.list:
+            label = elem.split(',')[1]
+            self.class_lists[tokens.find(label)].append(elem)
+
+
+        self.remove_unlisted = remove_unlisted
+        self.load = load
+        self.path = path
+        self.tokens = tokens
+        self.loadargs = kwargs
+
+    def __len__(self):
+        return len(self.list)
+
+    def __getitem__(self, idx):
+        num_classes = len(self.tokens)
+        class_idx = idx % num_classes
+        idx = int(idx / num_classes) % len(self.class_lists[class_idx])
+        ref = self.class_lists[class_idx][idx]
+
+        if self.path is not None:
+            return self.load("%s/%s" % (self.path, ref), tokens=self.tokens, **self.loadargs)
+
+        return self.load(self.list[idx], tokens=self.tokens, **self.loadargs)
+
+    def disjunct_split(self, ratio=.8):
+        # Split keeps the ratio of classes
+        A = set()
+        for cl in self.class_lists:
+            A.update(random.sample(cl, int(len(cl) * ratio)))
+        B = set(self.list) - A
+
+        A = DataSet(A, self.load, self.path, self.remove_unlisted,
+                    self.tokens, **self.loadargs)
+        B = DataSet(B, self.load, self.path, self.remove_unlisted,
+                    self.tokens, **self.loadargs)
+        return A, B
+'''
 
 def load_mat(ref, normalize=True):
     mat = loadmat(ref)
@@ -14,16 +94,17 @@ def load_mat(ref, normalize=True):
         #features = (features - features.mean()) / features.std()
         #features = (features - features.min()) / features.max()
 
-    return data #, features
+    return data #, features Crop:
+    def __init__(self, crop_len):
+        self.crop_len = crop_len
 
+    def __call__(self, data):
+        crop_len = self.crop_len
+        if len(data[0]) > crop_len:
+            start_idx = np.random.randint(len(data[0]) - crop_len)
+            data = data[:, start_idx: start_idx + crop_len]
+        return data
 
-def load_composed(line, transformations=[], **kwargs):
-    ref = line
-    data = load_mat(ref)
-    data_len = len(data[0])
-    for trans in transformations:
-        data = trans(data)
-    return data
 
 class Crop:
     def __init__(self, crop_len):
@@ -51,6 +132,7 @@ class Threshold:
             data[np.abs(data) > data.std()*self.sigma] = data.std()*self.sigma
         return data
 
+
 class RandomMultiplier:
     def __init__(self, multiplier=-1.):
         self.multiplier = multiplier
@@ -62,6 +144,7 @@ class Logarithm:
     def __call__(self, data):
         return np.log(data)
 
+
 class Spectogram:
     def __init__(self, NFFT=None, overlap=None):
         self.NFFT = NFFT
@@ -71,26 +154,40 @@ class Spectogram:
     def __call__(self, data):
         data = data.squeeze()
         assert len(data.shape) == 1
+        length = len(data)
         Sx = signal.spectrogram(
             x=data,
             nperseg=self.NFFT,
             noverlap=self.overlap)[-1]
-        return Sx    
-'''
-class Spectogram:
-    def __init__(self, NFFT, overlap=None):
-        self.NFFT = NFFT
-        self.overlap = overlap
-        if overlap is None:
-            self.overlap = NFFT / 2
-    def __call__(self, data):
-        data = data.squeeze()
-        assert len(data.shape) == 1
-        Sx = specgram(
-            x=data,
-            NFFT=self.NFFT,
-            Fs=300,
-            noverlap=self.NFFT/2,
-            window=np.hamming(self.NFFT))[0]
+        Sx = signal.resample(Sx, length, axis=1)
         return Sx
-'''
+
+
+def load_composed(line, transformations=[], **kwargs):
+    ref = line
+    data = load_mat(ref)
+    for trans in transformations:
+        data = trans(data)
+
+    
+    return data
+
+
+def load_forked(line, global_transforms, fork_transforms, **kwargs):
+    #ref, label = line.split(',')
+    ref = line
+    in_data = load_mat(ref)
+    for trans in global_transforms:
+        in_data = trans(in_data)
+    forks = {}
+    for forkname, transforms in fork_transforms.items():
+        data = in_data.copy()
+        for trans in transforms:
+            data = trans(data)
+        assert len(data.shape) < 3, data.shape
+        if len(data.shape) == 1:
+            data = data[None, :]
+        forks[forkname] = np.float32(data)
+    return forks
+
+
--- entry_eval_ppkeitk.py
+++ entry_eval_ppkeitk.py
@@ -4,7 +4,7 @@ import os
 import sys
 import numpy as np
 import tensorflow as tf
-import tensorflow_wrapper as tw
+import combined_wrapper as tw
 import data_handler
 
 def pprint(*text, **kwargs):
@@ -14,10 +14,10 @@ def process_logits(logits):
     noise, classes = logits
     if noise.argmax() == 0:
         return('~')
-    if classes.argmax() == 1:
-        return('A')
     if classes.argmax() == 0:
         return('N')
+    if classes.argmax() == 1:
+        return('A')
     return('O')
 
 
@@ -29,6 +29,7 @@ def main(argv):
     print('-'*80)
     print('*'*80)
     print('-'*80)
+
     noise_model_path = 'noise.pkl'
     class_model_path = 'class.pkl'
 
@@ -36,51 +37,43 @@ def main(argv):
     pprint('Building computational graph... ')
 
     with tf.device('cpu'):
-        input_op = tf.placeholder(1, [1, None, 1], name='INPUT')
-        noise_model = tw.get_logits(input_op, 3, noise_model_path)[0]
-        class_model = tw.get_logits(input_op, 3, class_model_path, res_blocks=9, init_channel=32)[0]
+        xtime = tf.placeholder(1, [1, None, 1])
+        xfreq = tf.placeholder(1, [1, None, 16])
+        noise_model = tw.get_logits(timeInput=xtime, freqInput=xfreq, pytorch_statedict_path=noise_model_path, res_blocks=3, num_classes=2)[0]
+        class_model = tw.get_logits(timeInput=xtime, freqInput=xfreq, pytorch_statedict_path=class_model_path, res_blocks=3, num_classes=3)[0]
         logits = [noise_model, class_model]
     pprint('Computational graph building done!')
     pprint('Loading data')
-    transformations = [
+    global_transforms = [
         #data_handler.Crop(6000),
-        data_handler.Threshold(sigma=2.2),
-        #data_handler.RandomMultiplier(-1),
     ]
-    data = data_handler.load_composed(
-        sys.argv[1], transformations=transformations)[:, :, None]
-    print(data.shape)
+
+    transTime = [
+        data_handler.Threshold(sigma=2.2),
+        data_handler.RandomMultiplier(-1),
+    ]
+
+    transFreq = [
+        data_handler.RandomMultiplier(-1),
+        data_handler.Spectogram(31),
+        #data_handler.Logarithm()
+    ]
+    
+    data = data_handler.load_forked(
+        sys.argv[1],
+        global_transforms=global_transforms,
+        fork_transforms={'time':transTime, 'freq':transFreq})
+    timeData = data['time'][:, :, None]
+    freqData = data['freq'][:, :, None].transpose(2, 1, 0)
+    print('Evaluation')
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
-        logits_val = sess.run(logits, {input_op: data})
+        logits_val = sess.run(logits, {xtime: timeData, xfreq:freqData})
         print('Noise:', logits_val[0])
         print('Class:', logits_val[1])
-    print(process_logits(logits_val))
+        print(process_logits(logits_val))
     write_answer(argv[1], process_logits(logits_val))
-    return
 
-    with tf.Session() as sess:
-        # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-        ckpt = tf.train.get_checkpoint_state('ckpt/%s/' % proto_name)
-        saver = tf.train.Saver()
-        if ckpt and ckpt.model_checkpoint_path:
-            # Restores from checkpoint
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            # Assuming model_checkpoint_path looks something like:
-            #   /my-favorite-path/cifar10_train/model.ckpt-0,
-            # extract global_step from it.
-            global_step =\
-                ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-            pprint('Checkpoint loaded at train step:', global_step)
-        else:
-            pprint('No checkpoint file found')
-            return
-
-        
-        sample = loadmat(argv[1])
-        feed = {'is_training:0': False, input_op: sample['val']}
-        logits_val = sess.run(logits, feed)
-        write_answer(argv[1], logits_val)
     print('-'*80)
     print('*'*80)
     print('-'*80)
